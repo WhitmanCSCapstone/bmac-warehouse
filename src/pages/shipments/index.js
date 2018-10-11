19be92335c627a7d07c2f3564896bfae867cc58f
Ben Limpich <benlimpich@gmail.com>
2018-10-11 11:09:59 -0700

    make shipments page work w/new Firebase keys

--- src/pages/shipments/index.js
+++ src/pages/shipments/index.js
@@ -5,7 +5,6 @@
 import React from 'react';
 import firebase from '../../firebase.js';
 import ReactTable from 'react-table';
-import { Button } from 'antd' ;
 import LoadingScreen from '../../components/LoadingScreen';
 
 const styles = {
@@ -17,10 +16,17 @@ const styles = {
 };
 
 const keys = [
-  'Customer',
-  'Product',
-  'Ship Date',
-  'Weight'
+    "customer_id",
+    "funds_source",
+    "ship_date",
+    "ship_via",
+    "ship_items",
+    "ship_rate",
+    "total_weight",
+    "total_price",
+    "invoice_date",
+    "invoice_no",
+    "notes"
 ];
 
 class Shipments extends React.Component {
@@ -33,7 +39,7 @@ class Shipments extends React.Component {
 
   componentDidMount(){
     var database = firebase.database();
-    var shipmentsRef = database.ref('shipments')
+    var shipmentsRef = database.ref('2/shipments')
     shipmentsRef.on('value', (snapshot) => {
       var ship = snapshot.val()
       this.setState({
@@ -45,23 +51,19 @@ class Shipments extends React.Component {
   render() {
     return(
       <div style={styles.container}>
-        {!this.state.data ? <LoadingScreen/> :
-
-         <ReactTable
-             data={this.state.data  ? this.state.data : []}
-             columns={keys.map(string => {
-             return({
-             Header: string,
-             accessor: string,
-             })
-             })}
-
-             defaultPageSize={10}
-             className="-striped -highlight"
-         />
-
+        { !this.state.data ? <LoadingScreen/> :
+          <ReactTable
+            data={this.state.data ? this.state.data : []}
+            columns={keys.map(string => {
+                return({
+                  Header: string,
+                  accessor: string,
+                })
+            })}
+            defaultPageSize={10}
+            className="-striped -highlight"
+          />
         }
-
       </div>
     );
   }
