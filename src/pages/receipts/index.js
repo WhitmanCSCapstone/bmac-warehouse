1ff6e84c031c1afc27c41cbff10df9588c76e501
Ben Limpich <benlimpich@gmail.com>
2018-10-16 17:16:20 -0700

    add table to receipts page

--- src/pages/receipts/index.js
+++ src/pages/receipts/index.js
@@ -3,23 +3,62 @@
  */
 
 import React from 'react';
+import firebase from '../../firebase.js';
+import ReactTable from 'react-table';
+import LoadingScreen from '../../components/LoadingScreen';
 
 const styles = {
   container: {
+    flexGrow: 1,
+    display: "flex",
+    flexDirection: "column",
   },
 };
+
+const keys = [
+  "provider_id",
+  "recieve_date",
+  "receive_items",
+  "payment_source",
+  "billed_amt",
+  "notes"
+];
 
 class Receipts extends React.Component {
   constructor(props){
     super(props);
     this.state = {
+      data: null,
     }
+  }
+
+  componentDidMount(){
+    var database = firebase.database();
+    var shipmentsRef = database.ref('6/contributions')
+    shipmentsRef.on('value', (snapshot) => {
+      var ship = snapshot.val()
+      this.setState({
+        data: ship
+      })
+    });;
   }
 
   render() {
     return(
       <div style={styles.container}>
-        This is the receipts page!
+        { !this.state.data ? <LoadingScreen/> :
+          <ReactTable
+            data={this.state.data ? this.state.data : []}
+            columns={keys.map(string => {
+                return({
+                  Header: string,
+                  accessor: string,
+                })
+            })}
+            defaultPageSize={10}
+            className="-striped -highlight"
+          />
+        }
       </div>
     );
   }
