cfffdea67ccf43c50ce9acc4a45e67dc1311939b
Ben Limpich <benlimpich@gmail.com>
2018-11-04 18:27:12 -0800

    add rudimentary reports for shipments and receipts

--- src/pages/reports/index.js
+++ src/pages/reports/index.js
@@ -3,23 +3,262 @@
  */
 
 import React from 'react';
+import firebase from '../../firebase.js';
+import ReactTable from 'react-table';
+import { tableKeys, reportKeys } from '../../constants';
+import { Spin, Dropdown, Button, Icon, DatePicker, Radio, Menu } from 'antd';
+import { reportType2TableName,
+         reportType2DateAccessor,
+         reportType2FundingSourceRelavancy,
+         reportType2DateRangeRelavancy,
+         radioValue2ReportType } from '../../constants';
+import { populateTableData,
+         getCSVdata,
+         filterDataByDate,
+         cleanFundingSourcesData } from './utils';
+import { CSVLink, CSVDownload } from "react-csv";
+
+const antIcon = <Icon type="loading" style={{ fontSize: '1rem', color: 'white' }} spin />;
+const { RangePicker } = DatePicker;
 
 const styles = {
   container: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  filters: {
+    display: 'flex',
+  },
+  radio: {
+    display: 'flex',
+    flexDirection: 'column',
   },
 };
+
+
+
+
+// receipts report is done except it doesn't do weight properly
+// figure out which reports actually need to be implemented
+// see where you need the active inactive/discontinued radio to be clickable
+
+
+
 
 class Reports extends React.Component {
   constructor(props){
     super(props);
     this.state = {
+      reportType: 'Inventory Shipments',
+      reportTypeTableName: 'shipments',
+      data: null,
+      fundingSource: null,
+      dateRange: [],
+      reportTypeRadioValue: 1,
+      statusRadioValue: 6,
+      fundingSources: [],
+      dataCSV: null,
     }
   }
 
+  componentDidMount(){
+    console.log('componentDidMount');
+    var database = firebase.database();
+    var ref = database.ref('4/fundingsources')
+    ref.on('value', (snapshot) => {
+      var data = snapshot.val();
+      data = cleanFundingSourcesData(data);
+      this.setState({
+        fundingSources: data
+      })
+    });;
+
+    this.updateTable();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('component did update');
+    if (this.state.reportTypeTableName !== prevState.reportTypeTableName
+        || this.state.fundingSource !== prevState.fundingSource
+        || this.state.dateRange !== prevState.dateRange) {
+      this.updateTable();
+      //this.setState({dataCSV: null});
+    }
+  }
+
+  updateTable = () => {
+    console.log('updateTable fired');
+    populateTableData(this.state.reportTypeTableName,
+                      this.state.fundingSource,
+                      this.state.dateRange,
+                      reportType2DateAccessor[this.state.reportType],
+                      (data) => {this.setState({data: data}, console.log('just updated table data'))});
+  }
+
+  createCSV = () => {
+    console.log('createCSV fired');
+    getCSVdata(this.state.data, this.state.reportTypeTableName, (dataCSV) => {this.setState({dataCSV: dataCSV}), console.log('just updated csv data')});
+  }
+
+  onClickFundingSource = (e) => {
+    console.log('onClickFundingSource fired');
+    this.setState({
+      fundingSource: e.key,
+    });
+  }
+
+  clearFundingSource = () => {
+    console.log('clearFundingSource fired');
+    this.setState({
+      fundingSource: null,
+    });
+  }
+
+  onReportTypeChange = (e) => {
+    console.log('onReportTypeChange fired');
+    var val = e.target.value.toString();
+    var reportType = radioValue2ReportType[val];
+    var tableName = reportType2TableName[reportType];
+    this.setState({
+      reportType: reportType,
+      reportTypeTableName: tableName,
+      reportTypeRadioValue: e.target.value,
+      //reset all the settings
+      data: null,
+      fundingSource: null,
+      dateRange: [],
+      dataCSV: null,
+    });
+  }
+
+  onDateChange = (dateRange) => {
+    console.log('onDateChange fired');
+    this.setState({
+      dateRange: dateRange,
+    });
+  }
+
+  onStatusChange = (e) => {
+    console.log('onStatusChange');
+    this.setState({
+      statusRadioValue: e.target.value,
+    });
+  }
+
   render() {
+
+    console.log('render fired');
+
+    var menu = (
+      <Menu>
+        {this.state.fundingSources.map((name) => {
+           return(
+             <Menu.Item key={name} onClick={this.onClickFundingSource}>
+               {name}
+             </Menu.Item>
+           )
+        })}
+        <Menu.Item key='None' onClick={this.clearFundingSource}>
+          <strong>Any</strong>
+        </Menu.Item>
+      </Menu>
+    )
+
+    var fundingSourceDisabled = !reportType2FundingSourceRelavancy[this.state.reportType];
+
     return(
       <div style={styles.container}>
-        This is the Reports page!
+
+        <div style={styles.filters}>
+          <Radio.Group onChange={this.onReportTypeChange}
+                       value={this.state.reportTypeRadioValue}
+                       style={styles.radio}>
+
+            <Radio value={1}>Inventory Shipments</Radio>
+            <Radio value={2}>Inventory Receipts</Radio>
+            <Radio value={3}>Current Inventory</Radio>
+            <Radio value={4}>Current Customers</Radio>
+            <Radio value={5}>Current Providers</Radio>
+
+          </Radio.Group>
+
+          <Radio.Group onChange={this.onStatusChange}
+                       value={this.state.statusRadioValue}
+                       style={styles.radio}>
+
+            <Radio disabled={true} value={6}>Active</Radio>
+            <Radio disabled={true} value={7}>Inactive/Discontinued</Radio>
+
+          </Radio.Group>
+
+          {
+
+            <div>
+              <Dropdown disabled={fundingSourceDisabled} overlay={menu}>
+                <a className="ant-dropdown-link" href="#">
+                  <Button disabled={fundingSourceDisabled}>
+                    {this.state.fundingSource
+                     ? this.state.fundingSource
+                     : <span>Funding Source</span> } <Icon type="down" />
+                  </Button>
+                </a>
+              </Dropdown>
+            </div>
+          }
+
+          {
+            <RangePicker onChange={this.onDateChange}
+                         value={this.state.dateRange}
+                         disabled={!reportType2DateRangeRelavancy[this.state.reportType]}
+            />
+          }
+
+          {
+            this.state.data
+            ?
+            <Button type="primary" onClick={this.createCSV}>Create CSV</Button>
+            : <Button type="primary"> Create CSV <Spin indicator={antIcon} />
+            </Button>
+          }
+
+          {
+            this.state.dataCSV
+            ? <CSVLink filename={'report.csv'} data={this.state.dataCSV}>
+              <Button type="primary" icon="download">
+                CSV
+              </Button>
+            </CSVLink>
+            : <Button type="primary">
+              CSV <Spin indicator={antIcon} />
+            </Button>
+          }
+
+        </div>
+
+        <ReactTable
+          data={this.state.dataCSV ? this.state.dataCSV : []}
+          columns={reportKeys[this.state.reportTypeTableName].map(string => {
+              return({
+                Header: string,
+                accessor: string,
+              })
+          })}
+          defaultPageSize={10}
+          className="-striped -highlight"
+        />
+
+        <ReactTable
+          data={this.state.data ? this.state.data : []}
+          columns={tableKeys[this.state.reportTypeTableName].map(string => {
+              return({
+                Header: string,
+                accessor: string,
+              })
+          })}
+          defaultPageSize={10}
+          className="-striped -highlight"
+        />
+
       </div>
     );
   }
---
+++ src/pages/reports/utils.js
@@ -0,0 +1,152 @@
+import Moment from 'moment';
+import firebase from '../../firebase.js';
+import { tableName2FirebasePath } from '../../constants';
+
+function createDictOfShipmentsSortedByProduct(data) {
+  var dict = {};
+  for (var i = 0; i < data.length; i++) {
+    var obj = data[i];
+    var items = obj['ship_items'];
+    // b/c ship_items sometimes doesn't exist
+    if (items) {
+      for (var n = 0; n < items.length; n++) {
+        var item = items[n];
+        var product = item['product'];
+        if (product in dict) {
+          dict[product].push(obj);
+        } else {
+          dict[product] = [obj];
+        }
+      }
+    }
+  }
+  return dict;
+}
+
+function createArrayOfShipmentsFromDict(dict) {
+  var array = [];
+  var keys = Object.keys(dict);
+  for (var i = 0; i < keys.length; i++) {
+    var key = keys[i];
+    var objs = dict[key];
+    for (var n = 0; n < objs.length; n++) {
+      var obj = objs[n];
+      var ship_date = obj['ship_date'];
+      var customer = obj['customer_id'];
+      var weight = obj['total_weight'];
+      var element = {product: key, ship_date: ship_date, customer: customer, weight: weight};
+      array.push(element);
+    }
+  }
+  return array;
+}
+
+function createDictOfReceiptsSortedByProduct(data) {
+  console.log('createDictOfReceiptsSortedByProduct fired');
+  var dict = {};
+  for (var i = 0; i < data.length; i++) {
+    var obj = data[i];
+    var items = obj['receive_items'];
+    // b/c ship_items sometimes doesn't exist
+    if (items) {
+      for (var n = 0; n < items.length; n++) {
+        var item = items[n];
+        var product = item['product'];
+        if (product in dict) {
+          dict[product].push(obj);
+        } else {
+          dict[product] = [obj];
+        }
+      }
+    }
+  }
+  return dict;
+}
+
+function createArrayOfReceiptsFromDict(dict) {
+  console.log('createArrayOfReceiptsFromDict fired');
+  var array = [];
+  var keys = Object.keys(dict);
+  for (var i = 0; i < keys.length; i++) {
+    var key = keys[i];
+    var objs = dict[key];
+    for (var n = 0; n < objs.length; n++) {
+      var obj = objs[n];
+      var receive_date = obj['recieve_date'];
+      var provider = obj['provider_id'];
+      var weight = 'IOU a weight value';
+      var element = {product: key, receive_date: receive_date, provider: provider, weight: weight};
+      array.push(element);
+    }
+  }
+  return array;
+}
+
+export function getCSVdata(data, tableName, callback) {
+  console.log('getCSVdata firing');
+  if (data) {
+    var array = []
+    if(tableName === 'shipments'){
+      console.log('returning a shipments csv');
+      var dict = createDictOfShipmentsSortedByProduct(data);
+      array = createArrayOfShipmentsFromDict(dict);
+    } else if (tableName === 'receipts') {
+      console.log('returning a receipts csv');
+      var dict = createDictOfReceiptsSortedByProduct(data);
+      array = createArrayOfReceiptsFromDict(dict);
+    }
+    callback(array);
+  } else {
+    console.log('return []');
+    callback([]);
+  }
+}
+
+export function populateTableData(reportTypeTableName, fundingSource, dateRange, date_accessor, callback) {
+  console.log('populating table data');
+  var database = firebase.database();
+  var path = tableName2FirebasePath[reportTypeTableName];
+  var ref = database.ref(path);
+
+  ref.on('value', (snapshot) => {
+    var data = snapshot.val();
+    data = Array.isArray(data) ? data : Object.keys(data).map((i) => {return(data[i])});
+    data = dateRange.length ? filterDataByDate(data, dateRange, date_accessor) : data;
+    data = fundingSource ? filterDataByFundingSource(data, fundingSource) : data;
+    callback(data);
+  });;
+}
+
+export function filterDataByFundingSource(data, fundingSource) {
+  console.log('filterDataByFundingSource fired');
+  var newData = [];
+  for (var i = 0; i < data.length; i++){
+    var entry = data[i];
+    var entryFS = entry['funds_source'];
+    if(entryFS === fundingSource){
+      newData.push(entry);
+    }
+  }
+  return newData;
+}
+
+export function filterDataByDate (data, dateRange, accessor) {
+  console.log('filterDataByDate fired');
+  var newData = [];
+  for (var i = 0; i < data.length; i++){
+    var entry = data[i];
+    var entryDate = Moment(entry[accessor], 'YY-MM-DD:HH:mm');
+    if(entryDate >= dateRange[0] && entryDate <= dateRange[1]){
+      newData.push(entry);
+    }
+  }
+  return newData;
+}
+
+export function cleanFundingSourcesData (data) {
+  var newData = [];
+  for (var i = 0; i < data.length; i++) {
+    newData.push(data[i]['id']);
+  }
+  return newData;
+}
