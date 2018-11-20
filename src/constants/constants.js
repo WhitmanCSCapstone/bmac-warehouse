ed5ea9abea0b5dd9bdda8d19bec76bd76b25d4f7
Jules Choquart <julochoq@gmail.com>
2018-11-20 14:45:17 -0800

    changed table fields

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -4,30 +4,30 @@ export const tableKeys = {
 
   shipments: [
     "customer_id",
-    "funds_source",
+    //"funds_source",
     "ship_date",
-    "ship_via",
-    "ship_rate",
-    "total_weight",
-    "total_price",
-    "invoice_date",
-    "invoice_no",
-    "notes"
+    //"ship_via",
+    //"ship_rate",
+    //"total_weight",
+    //"total_price",
+    //"invoice_date",
+    //"invoice_no",
+    //"notes"
   ],
 
   customers: [
     "customer_id",
-    "code",
+    //"code",
     "address",
-    "city",
-    "state",
-    "zip",
-    "county",
-    "contact",
+    //"city",
+    //"state",
+    //"zip",
+    //"county",
+    //"contact",
     "phone",
-    "email",
-    "status",
-    "notes"
+    //"email",
+    //"status",
+    //"notes"
   ],
 
   receipts: [
@@ -40,51 +40,51 @@ export const tableKeys = {
 
   products: [
     "product_id",
-    "product_code",
+    //"product_code",
     "funding_source",
     "unit_weight",
-    "unit_price",
+    //"unit_price",
     "initial_date",
-    "initial_stock",
-    "minimum_stock",
-    "history",
-    "current_stock",
-    "inventory_date",
+    //"initial_stock",
+    //"minimum_stock",
+    //"history",
+    //"current_stock",
+    //"inventory_date",
     "status",
-    "notes"
+    //"notes"
   ],
 
   providers: [
     "provider_id",
-    "code",
-    "type",
+    //"code",
+    //"type",
     "address",
-    "city",
-    "state",
-    "zip",
-    "county",
-    "contact",
+    //"city",
+    //"state",
+    //"zip",
+    //"county",
+    //"contact",
     "phone",
-    "email",
-    "status",
-    "notes"
+    //"email",
+    //"status",
+    //"notes"
   ],
 
   staff: [
-    "unique_id",
-    "username",
+    //"unique_id",
+    //"username",
     "last_name",
     "first_name",
-    "address",
-    "city",
-    "zip",
+    //"address",
+    //"city",
+    //"zip",
     "phone1",
-    "phone2",
+   // "phone2",
     "email",
-    "type",
-    "status",
-    "notes",
-    "password"
+    //"type",
+    //"status",
+    //"notes",
+    //"password"
   ],
 }
 
