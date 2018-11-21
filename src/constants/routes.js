a0c338cdf6c2ed22e00459e5ffeb5bbe9d5e29c2
Jules Choquart <julochoq@gmail.com>
2018-11-20 21:23:27 -0800

    added enterKey for login and changed route from login to dashboard/home

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -4,3 +4,4 @@ export const LANDING = '/';
 export const DASHBOARD = '/dashboard';
 export const ACCOUNT = '/account';
 export const PASSWORD_FORGET = '/pw-forget';
+export const HOME = '/dashboard/home';
