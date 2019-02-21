b65f2b1a4c0050e41a2425197ab3a1b5c118c4ba
Ben Limpich <benlimpich@gmail.com>
2019-02-20 17:34:45 -0800

    simplify Customer and Provider AutoCompletes

--- src/components/form/CustomerAutoComplete.js
+++ src/components/form/CustomerAutoComplete.js
@@ -12,8 +12,8 @@ class CustomerAutoComplete extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      data: [],
-      selectedHash: null,
+      dataSourceTypeItemList: [],
+      dictionary: {},
     }
   }
 
@@ -21,38 +21,32 @@ class CustomerAutoComplete extends React.Component {
     db.onceGetCustomers().then(snapshot => {
 
       var data = snapshot.val();
-      var names = [];
-      var dictionary = {};
-      for(let [key, value] of Object.entries(data)){
+      var dictionary = {}
+      var dataSourceTypeItemList = [];
 
+      for(let [key, value] of Object.entries(data)) {
         let name = value['customer_id'];
-
-        if(name in dictionary || name === ""){
-          console.log(`theres a dupe or an empty string in the customers table that should be deleted, here's its customer_id value: ${name}`);
-        } else {
-          dictionary[name] = key;
-          names.push(name);
-        }
+        dataSourceTypeItemList.push({value: key, text: name});
+        dictionary[key] = name;
       }
 
       this.setState({
+        dataSourceTypeItemList: dataSourceTypeItemList,
         dictionary: dictionary,
-        names: names,
       });
 
     });
   }
 
   onChange = (val) => {
-    var hash = this.state.dictionary[val];
-    this.props.onCustomerChange(hash);
+    this.props.onCustomerChange(val);
   }
 
   render() {
     return(
       <AutoComplete
-        dataSource={this.state.names}
-        defaultValue={this.props.rowData ? this.props.rowData.customer_id : undefined }
+        dataSource={this.state.dataSourceTypeItemList}
+        key={this.state.defaultValue}
         style={styles.container}
         onChange={ this.onChange }
         placeholder="Customer"
