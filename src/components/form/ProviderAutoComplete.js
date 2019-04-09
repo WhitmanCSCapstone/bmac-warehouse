b33f8a9d96dd37d1feca7b5e8c148c63433cd42e
Ben Limpich <benlimpich@gmail.com>
2019-04-09 15:24:14 -0700

    make receipt form popup and fill-in when receipt is clicked on

--- src/components/form/ProviderAutoComplete.js
+++ src/components/form/ProviderAutoComplete.js
@@ -13,25 +13,34 @@ class ProviderAutoComplete extends React.Component {
     super(props);
     this.state = {
       dataSourceTypeItemList: null,
+      dictionary: {},
+      defaultValue: null,
     }
   }
 
   componentDidMount(){
     db.onceGetProviders().then(snapshot => {
+
       var data = snapshot.val();
+      var dictionary = {}
       var dataSourceTypeItemList = [];
+
       for(let [key, value] of Object.entries(data)) {
         let name = value['provider_id'];
         dataSourceTypeItemList.push({value: key, text: name});
+        dictionary[key] = name;
       }
+
       this.setState({
         dataSourceTypeItemList: dataSourceTypeItemList,
+        dictionary: dictionary,
+        defaultValue: this.props.rowData ? dictionary[this.props.rowData.provider_id] : null,
       });
     });
   }
 
   onChange = (val) => {
-    this.props.onProviderChange('provider_id', val);
+    this.props.onProviderChange(val);
   }
 
   render() {
@@ -39,6 +48,8 @@ class ProviderAutoComplete extends React.Component {
       <AutoComplete
         dataSource={this.state.dataSourceTypeItemList}
         style={styles.container}
+        defaultValue={this.props.rowData ? this.props.rowData.provider_id : undefined }
+        key={this.state.defaultValue}
         onChange={ this.onChange }
         placeholder="Provider"
         filterOption={(inputValue, option) => option.props.children.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1}
