aa2e4d806c7e918ba7bfe6f02152a0fd41319681
Ben Limpich <benlimpich@gmail.com>
2019-04-17 12:01:55 -0700

    fix Dashboard.js indentation

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -20,139 +20,139 @@ import {Layout, Menu} from 'antd';
 const {Header, Content, Footer} = Layout;
 
 const styles = {
-    layout: {
-        display: "flex",
-        flex: "column",
-        minHeight: "100vh"
-    },
+  layout: {
+    display: "flex",
+    flex: "column",
+    minHeight: "100vh"
+  },
 
-    header: {
-        display: "flex",
-        backgroundColor: "white",
-        //marginBottom: "auto",
-        width: "100%",
-        borderBottom: "1px solid #EBEDF0"
-    },
+  header: {
+    display: "flex",
+    backgroundColor: "white",
+    //marginBottom: "auto",
+    width: "100%",
+    borderBottom: "1px solid #EBEDF0"
+  },
 
-    footer: {
-        width: "100%",
-        padding: "1em",
-        alignSelf: "flex-end",
-        backgroundColor: "white",
-        borderTop: "1px solid #EBEDF0",
-        fontSize: "small",
-        color: "#595959",
-        textAlign: "center"
-    },
+  footer: {
+    width: "100%",
+    padding: "1em",
+    alignSelf: "flex-end",
+    backgroundColor: "white",
+    borderTop: "1px solid #EBEDF0",
+    fontSize: "small",
+    color: "#595959",
+    textAlign: "center"
+  },
 
-    content: {
-        display: "flex",
-        flexDirection: "column",
-        backgroundColor: "#F0F2F5",
-        //marginTop: "auto",
-    },
+  content: {
+    display: "flex",
+    flexDirection: "column",
+    backgroundColor: "#F0F2F5",
+    //marginTop: "auto",
+  },
 
-    menu: {
-        display: "flex",
-        flexWrap: "wrap",
-        marginTop: "1%"
-    },
+  menu: {
+    display: "flex",
+    flexWrap: "wrap",
+    marginTop: "1%"
+  },
 
-    title: {
-        paddingRight: "1%",
-        fontSize: "2em"
-    },
+  title: {
+    paddingRight: "1%",
+    fontSize: "2em"
+  },
 
-    signOutButton: {
-        paddingTop: ".4%"
-    }
+  signOutButton: {
+    paddingTop: ".4%"
+  }
 
 }
 
 const pages = {
-    home: Home,
-    shipments: Shipments,
-    receipts: Receipts,
-    products: Products,
-    staff: Staff,
-    providers: Providers,
-    customers: Customers,
-    reports: Reports,
-    about: About,
-    help: Help,   
+  home: Home,
+  shipments: Shipments,
+  receipts: Receipts,
+  products: Products,
+  staff: Staff,
+  providers: Providers,
+  customers: Customers,
+  reports: Reports,
+  about: About,
+  help: Help,
 }
 
 class Dashboard extends React.Component {
-    state = {
-        // this should be "home" but currently the app doesn't auto-route to the home
-        // page, so for now this is null
-        current: null
-    }
+  state = {
+    // this should be "home" but currently the app doesn't auto-route to the home
+    // page, so for now this is null
+    current: null
+  }
 
-    componentDidMount() {
-        console.log('Dashboard Mounted');
-    }
+  componentDidMount() {
+    console.log('Dashboard Mounted');
+  }
 
-    render() {
+  render() {
 
-        const {match} = this.props // coming from React Router.
+    const {match} = this.props // coming from React Router.
 
-        return (
-            <Router>
+    return (
+      <Router>
 
-                <Layout style={styles.layout}>
+        <Layout style={styles.layout}>
 
-                    <Header style={styles.header}>
-                        <em style={styles.title}>BMAC-Warehouse</em>
-                        <Menu
-                            onClick={this.handleClick}
-                            selectedKeys={[this.state.current]}
-                            mode="horizontal"
-                            theme="light"
-                            style={styles.menu}>
-                            {Object
-                                .keys(pages)
-                                .map((name) => {
-                                    return (
-                                        <Menu.Item key={name}>
-                                            <Link to={`${match.url}/${name}`}>
-                                                {name
-                                                    .charAt(0)
-                                                    .toUpperCase() + name.slice(1)}
-                                            </Link>
-                                        </Menu.Item>
-                                    );
-                                })}
-                        </Menu>
-                        <div style={styles.signOutButton}>
-                            <SignOutButton type="danger"/>
+          <Header style={styles.header}>
+            <em style={styles.title}>BMAC-Warehouse</em>
+            <Menu
+              onClick={this.handleClick}
+              selectedKeys={[this.state.current]}
+              mode="horizontal"
+              theme="light"
+              style={styles.menu}>
+              {Object
+                .keys(pages)
+                .map((name) => {
+                  return (
+                    <Menu.Item key={name}>
+                      <Link to={`${match.url}/${name}`}>
+                        {name
+                          .charAt(0)
+                          .toUpperCase() + name.slice(1)}
+                      </Link>
+                    </Menu.Item>
+                  );
+                })}
+            </Menu>
+            <div style={styles.signOutButton}>
+              <SignOutButton type="danger"/>
 
-                        </div>
+            </div>
 
-                    </Header>
+          </Header>
 
-                    <Content style={styles.content}>
-                        <Switch>
-                            {Object
-                                .keys(pages)
-                                .map((name) => {
-                                    return (<Route exact path={`${match.path}/${name}`} component={pages[name]} key={name}/>)
-                                })}
-                        </Switch>
-                    </Content>
+          <Content style={styles.content}>
+            <Switch>
+              {Object
+                .keys(pages)
+                .map((name) => {
+                  return (<Route exact path={`${match.path}/${name}`} component={pages[name]} key={name}/>)
+                })}
+            </Switch>
+          </Content>
 
-                    <Footer style={styles.footer}>
-                        Whitman Capstone Project 2019
-                        <br/>
-                        Copyright ©2018 Rajesh Narayan, Paul Milloy, Ben Limpich, Jules Choquart, and
-                        Pablo Fernandez
-                    </Footer>
+          <Footer style={styles.footer}>
+            Whitman Capstone Project 2019
+            <br/>
+            Copyright ©2018 Rajesh Narayan, Paul Milloy, Ben Limpich, Jules Choquart, and
+            Pablo Fernandez
+          </Footer>
 
-                </Layout>
+        </Layout>
 
-            </Router>
-        );
-    }
+      </Router>
+    );
+  }
 };
 
 const authCondition = (authUser) => !!authUser;
