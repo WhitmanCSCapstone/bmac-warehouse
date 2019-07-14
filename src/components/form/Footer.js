8e3c6723f37235827bdbc5b1789b0535f948aa46
Ben Limpich <benlimpich@gmail.com>
2019-07-13 17:48:32 -0700

    close #196 and adds some nice icons to the Footer

--- src/components/form/Footer.js
+++ src/components/form/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button } from 'antd';
+import { Button, Icon } from 'antd';
 
 const styles = {
   container: {
@@ -41,19 +41,21 @@ class Footer extends React.Component {
 
         {this.props.handleLabelClick ? (
           <Button key="savelabel" type="primary" onClick={this.props.handleLabelClick}>
-            Create Label
+            Label
+            <Icon type="file-pdf" />
           </Button>
         ) : null}
 
         {this.props.handleInvoiceClick ? (
           <Button key="saveInvoice" type="primary" onClick={this.props.handleInvoiceClick}>
-            Create Invoice
+            Invoice <Icon type="file-pdf" />
           </Button>
         ) : null}
 
         {this.props.handleReceiptClick ? (
           <Button key="saveReceipt" type="primary" onClick={this.props.handleReceiptClick}>
-            Create Receipt
+            Receipt
+            <Icon type="file-pdf" />
           </Button>
         ) : null}
 
@@ -61,7 +63,7 @@ class Footer extends React.Component {
           Cancel
         </Button>
         <Button key="submit" loading={this.state.loading} type="primary" onClick={this.onClick}>
-          Submit
+          Save
         </Button>
       </div>
     );
